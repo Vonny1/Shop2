50980c2eefac8649c5104f0dfc3fa01431631468
Ефимов Владислав Сергеевич <Vladislav.Efimov@main.russianpost.ru>
2021-05-27 15:23:42 +0700

    Added GET page, GET Category, changed routing

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shop2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shop2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        databaseContext db = new databaseContext();
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> Get()
+        {
+            return await db.Categories.ToListAsync();
+        }
+
+    }
+}
